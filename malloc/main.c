65beab475b28223004a7d61c71724680954ed882
Tom Randolph <thomas.randolph26@gmail.com>
2024-06-04 11:09:28 -0700

    basic DLL freelist

--- malloc/main.c
+++ malloc/main.c
@@ -5,18 +5,38 @@
 struct malloc_chunk
 {
     size_t size;
-    bool inuse;
     struct malloc_chunk *next;
     struct malloc_chunk *prev;
 } typedef malloc_chunk;
 
 typedef malloc_chunk *mchunkptr;
 
-static void *heap = NULL;
 static size_t heap_size = 0;
-static mchunkptr freelist;
-static mchunkptr all_chunks;
+
+static malloc_chunk free_list_tail =
+    {
+        .size = 0,
+        .next = NULL,
+        .prev = NULL};
+static malloc_chunk free_list_head =
+    {
+        .size = 0,
+        .next = NULL,
+        .prev = NULL};
+
+#define can_split_chunk(chunk, size) (chunk->size >= size + sizeof(malloc_chunk))
+#define remove_chunk(chunk)              \
+    {                                    \
+        chunk->prev->next = chunk->next; \
+        chunk->next->prev = chunk->prev; \
+    }
 #define HEAP_SIZE 4096
+
+#define assert(cond, msg) \
+    {                     \
+        if (!(cond))      \
+            fail(msg);    \
+    }
 
 #define fail(msg)           \
     {                       \
@@ -26,114 +46,123 @@ static mchunkptr all_chunks;
 
 #define move_ptr(ptr, size) ((void *)((char *)(ptr) + (size)))
 
-void init_heap()
+void expand_heap()
 {
-    void *addr = mmap(heap, HEAP_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    static void *heap = NULL;
+    printf("Expanding heap...");
+    void *start_ptr = heap == NULL ? NULL : move_ptr(heap, heap_size);
+    void *addr = mmap(start_ptr, HEAP_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (heap == NULL)
+        heap = addr;
     if (addr == MAP_FAILED)
         fail("mmap");
-    heap = addr;
     heap_size += HEAP_SIZE;
-    printf("Heap initialized at %p\n", heap);
-    mchunkptr chunk = (mchunkptr)heap;
-    chunk->size = HEAP_SIZE - sizeof(malloc_chunk);
-    chunk->next = NULL;
-    freelist = chunk;
-    all_chunks = chunk;
+    printf("Heap expanded at %p\n", addr);
+    mchunkptr chunk = (mchunkptr)addr;
+    chunk->size = HEAP_SIZE - sizeof(size_t);
+    free_list_tail.prev->next = chunk;
+    chunk->prev = free_list_tail.prev;
+    free_list_tail.prev = chunk;
+    chunk->next = &free_list_tail;
     printf("ok\n");
 }
 
 void *heap_alloc(size_t size)
 {
-    // Basic algorithm
-    // 1. Search freelist for chunk of size >= size
-    // 2. If found, split the chunk, update the freelist and return the address
-    // 3. If not found, request more memory from the OS
-    // 4. Go to step 2
-    if (heap == NULL)
+    if (free_list_head.next == NULL)
     {
-        printf("Uninitialized heap\n");
-        init_heap();
+        printf("Initializing heap\n");
+        free_list_head.next = &free_list_tail;
+        free_list_tail.prev = &free_list_head;
+        expand_heap();
     }
-    mchunkptr chunk = freelist;
-    // printf("chunk: %p\n", chunk);
-    size_t chunk_size = chunk->size;
-    // printf("chunk size: %u, size: %d remainder: %d\n", chunk_size, size, chunk_size - size);
-    size_t required_size = size + sizeof(malloc_chunk);
-    if (chunk_size < required_size)
+    if (free_list_head.next == &free_list_tail)
     {
-        printf("Not enough memory. remaining: %zu required: %zu+%zu=%zu\n", chunk_size, size, sizeof(malloc_chunk), required_size);
+        printf("No free chunks\n");
+        expand_heap();
+    }
+    mchunkptr chunk = free_list_head.next;
+
+    while (chunk->next != &free_list_tail && (chunk->size < size))
+    {
+        printf("Skipping chunk\n");
+        chunk = chunk->next;
+    }
+
+    if (!can_split_chunk(chunk, size))
+    {
+        printf("Cannot split chunk. filling\n");
+        remove_chunk(chunk);
+        size_t *size_header = (size_t *)chunk;
+        *size_header = chunk->size;
+        return move_ptr(chunk, sizeof(size_t));
+    }
+    printf("Splitting chunk\n");
+
+    mchunkptr chunk_prev = chunk->prev;
+    mchunkptr chunk_next = chunk->next;
+    size_t chunk_size = chunk->size;
+    void *chunk_data = move_ptr(chunk, sizeof(size_t));
+
+    mchunkptr next_chunk = (mchunkptr)move_ptr(chunk_data, size);
+    size_t remaining_space = chunk_size - size;
+
+    if (remaining_space <= sizeof(malloc_chunk))
+    {
+        printf("Out of memory: Remaining space %zu\n", remaining_space);
         return NULL;
     }
+    next_chunk->size = chunk_size - size - sizeof(size_t); // TODO use pointer arithmetic instead?
 
-    void *data = move_ptr(chunk, sizeof(malloc_chunk));
-    chunk->size = size;
-    chunk->inuse = true;
+    next_chunk->prev = chunk_prev;
+    next_chunk->next = chunk_next;
+    chunk_prev->next = next_chunk;
+    chunk_next->prev = next_chunk;
 
-    mchunkptr next_chunk = (mchunkptr)move_ptr(data, size);
-    next_chunk->size = chunk_size - required_size;
-    next_chunk->prev = chunk;
-    next_chunk->inuse = false;
-
-    chunk->next = next_chunk;
-    next_chunk->next = NULL;
-    next_chunk->prev = chunk;
-
-    freelist = next_chunk;
-    return data;
-}
-
-void heap_free(void *ptr)
-{
-    mchunkptr chunk = (mchunkptr)move_ptr(ptr, -sizeof(malloc_chunk));
-    chunk->inuse = false;
+    size_t *size_header = (size_t *)chunk;
+    *size_header = chunk_size;
+    return chunk_data;
 }
 
 #define PVAL 10
 #define QVAL 20
 #define bool2str(b) (b ? "X" : "O")
-void dump_heap()
-{
-    printf("Heap dump\n");
-    mchunkptr chunk = all_chunks;
-    int i = 1;
-    while (chunk != NULL)
-    {
-        printf("| %s %zu ", bool2str(chunk->inuse), chunk->size);
-        if (!(i % 8))
-            printf("\n");
-        chunk = chunk->next;
-        i += 1;
-    }
-    printf("\n");
-}
 
 int main()
 {
     int i = 1;
     u_int64_t total_allocated = 0;
 
-    while (1)
+    while (i < 4096)
     {
+        printf("Iteration %d\n", i);
         int *p = heap_alloc(sizeof(int));
-        int *q = heap_alloc(sizeof(int));
-        total_allocated += sizeof(int) * 2;
-        if (p == NULL || q == NULL)
+        if (p == NULL)
         {
             printf("Failed to allocate memory\n");
             break;
         }
+        total_allocated += sizeof(int);
+        int *q = heap_alloc(sizeof(int));
+        if (q == NULL)
+        {
+            printf("Failed to allocate memory\n");
+            break;
+        }
+        total_allocated += sizeof(int);
         printf("p: %p, q: %p\n", p, q);
         *p = PVAL;
         *q = QVAL;
-        if ((i % 3) == 0)
-        {
-            heap_free(p);
-            heap_free(q);
-        }
+
         i++;
-        // printf("(%d) p + q = %d. expected: %d. Success: %s\n", i, *p + *q, PVAL + QVAL, *p + *q == PVAL + QVAL ? "true" : "false");
+        printf("(%d) p + q = %d. expected: %d. Success: %s\n", i, *p + *q, PVAL + QVAL, *p + *q == PVAL + QVAL ? "true" : "false");
+        if (*p + *q != PVAL + QVAL)
+        {
+            printf("Memory corrupted\n");
+            break;
+        }
     }
-    dump_heap();
-    printf("Total allocated: %lu bytes using %zu with %.2f%%\n", total_allocated, HEAP_SIZE, 100 * (float)total_allocated / HEAP_SIZE);
+
+    printf("Total allocated: %llu bytes using %zu with %.2f%%\n", total_allocated, heap_size, 100 * (float)total_allocated / heap_size);
     return EXIT_SUCCESS;
 }
